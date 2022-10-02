0d89dcb1f5e53a6d2fb8ed940ed0ff5e18032126
Gustavo Santos Siqueira <gusiqueira2010@gmail.com>
2022-10-02 13:27:11 -0300

    feat: delete activity

--- app/src/main/java/com/example/appfirestore/DeleteActivity.kt
+++ app/src/main/java/com/example/appfirestore/DeleteActivity.kt
@@ -2,53 +2,71 @@ package com.example.appfirestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.*
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.view.View
 
 class DeleteActivity : AppCompatActivity() {
+    private lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delete)
 
         val db = Firebase.firestore
 
+        val btnDeletar: Button = findViewById(R.id.btnDeletar)
+        val btnConfirmar: Button = findViewById(R.id.btnConfirmar)
+
+        val txtPessoa: TextView = findViewById(R.id.txtPessoa)
+
+        val selectID: EditText = findViewById(R.id.edtSelecionarID)
         val ids: MutableList<String> = mutableListOf()
 
-        var selected: String = ""
+        var id: String = ""
+
 
         db.collection("cadastro").get().addOnSuccessListener {
-            result ->
+                result ->
             for(document in result) {
                 ids.add(document.id)
             }
         }
 
-        val btnDeletar: Button = findViewById(R.id.btnDeletar)
-
         btnDeletar.setOnClickListener {
-            if(selected.isEmpty()) {
-                Toast.makeText(this, "Por favor selecione um registro", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Registro ${selected} deletado", Toast.LENGTH_SHORT).show()
+            try{
+                if(selectID.text.toString().toInt() >= ids.size){
+                    Toast.makeText(this, "ID inválido", Toast.LENGTH_SHORT).show()
+                } else {
+                    id = ids[selectID.text.toString().toInt()]
+                    db.collection("cadastro").document(id).get().addOnSuccessListener {
+                            document ->
+                        if(document != null) {
+                            txtPessoa.text = getString(R.string.dados, id, document["nome"], document["cep"], document["bairro"], document["endereco"])
+                            btnConfirmar.visibility = View.VISIBLE
+                        } else {
+                            txtPessoa.text = "Pessoa não encontrada"
+                            btnConfirmar.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+            } catch (e: NumberFormatException) {
+                Toast.makeText(this, "ID inválido", Toast.LENGTH_SHORT).show()
             }
+
         }
 
-        val spinner: Spinner = findViewById(R.id.spinner)
-        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, ids)
-        spinner.adapter = adapter
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                selected = parent.getItemAtPosition(position).toString()
-
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                selected = ""
-            }
+        btnConfirmar.setOnClickListener {
+            db.collection("cadastro").document(id)
+            .delete()
+            .addOnSuccessListener { Toast.makeText(this, "Deletado com sucesso", Toast.LENGTH_SHORT).show() }
+            .addOnFailureListener { e -> Toast.makeText(this, "Erro: $e", Toast.LENGTH_SHORT).show() }
+            selectID.text.clear()
+            txtPessoa.text = ""
         }
     }
-}
+
+    }
